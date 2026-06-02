f0ce9716d95530d288482d194fa1fadef8856400
manoj1214 <manoj.yadav959882@gmail.com>
2026-06-02 22:51:54 +0530

    learning to Find the diviser of a number.

--- dsa/BasicMath.java
+++ dsa/BasicMath.java
@@ -428,3 +428,72 @@ class Solution {
         return reverse(reverse(num)) == num;
     }
 }
+
+
+
+
+**********Print all Divisors of a given Number   ****************
+/*
+Given an integer N, return all divisors of N.
+A divisor of an integer N is a positive integer that divides N without leaving a remainder.
+In other words, if N is divisible by another integer without any remainder, then that integer is considered a divisor of N.
+
+
+We can optimise the previous approach by using the property that for any non-negative integer n, if d is a divisor of n then n/d is also a divisor of n. 
+This property is symmetric about the square root of N. Thus, by traversing just the first half we can avoid redundant iteration and computations improving the efficiency of the algorithm.
+
+Iterate from 1 to sqrt(N) and for every divisor found, if N/divisor is distinct, add that to the list of divisors as well.
+*/
+
+
+import java.util.*;
+
+class Solution {
+    // Function to get all divisors
+    public List<Integer> getDivisors(int N) {
+        // Create a list to store divisors
+        List<Integer> res = new ArrayList<>();
+
+        // Loop from 1 to square root of N
+        for (int i = 1; i * i <= N; i++) {
+            // Check if i divides N
+            if (N % i == 0) {
+                // Add i to result
+                res.add(i);
+
+                // If N / i is different from i, add N / i too
+                if (i != N / i) {
+                    res.add(N / i);
+                }
+            }
+        }
+
+        // Return the list of divisors
+        return res;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create object of Solution class
+        Solution sol = new Solution();
+
+        // Input number
+        int N = 36;
+
+        // Get divisors
+        List<Integer> result = sol.getDivisors(N);
+
+        // Print the result
+        System.out.print("Divisors of " + N + ": ");
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+Complexity Analysis
+Time Complexity: O(sqrt(N)), we check for every number between 1 and sqaure root of N.
+Space Complexity: O(2*sqrt(N)), extra space used for storing divisors.
